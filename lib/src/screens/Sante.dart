6123914343a6dafd37fce925b5c659d1c07fe6c8
TheWanderWeeb <josefranciscosantesprimo@gmail.com>
2023-10-21 18:44:44 -0600

    Drawer con estilo y se corrigio un problema con las dimensiones

--- lib/src/screens/Sante.dart
+++ lib/src/screens/Sante.dart
@@ -21,10 +21,11 @@ class _SanteState extends State<Sante> {
         automaticallyImplyLeading: false,
         title: const Text("Sante"),
         leading: IconButton(
-            onPressed: () {
-              _globalKey.currentState?.openDrawer();
-            },
-            icon: const Icon(Icons.menu)),
+          onPressed: () {
+            _globalKey.currentState?.openDrawer();
+          },
+          icon: const Icon(Icons.menu),
+        ),
         elevation: 8,
         backgroundColor: Colors.transparent,
         flexibleSpace: Container(
@@ -32,7 +33,7 @@ class _SanteState extends State<Sante> {
             gradient: LinearGradient(
               colors: [
                 Color.fromRGBO(42, 157, 143, 1.0),
-                Color.fromRGBO(24, 90, 82, 1.0)
+                Color.fromRGBO(24, 90, 82, 1.0),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
@@ -40,25 +41,19 @@ class _SanteState extends State<Sante> {
           ),
         ),
         actions: [
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 15),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.search), // Icono del botón
-                  onPressed: () {
-                    // Acción al presionar el botón (puedes colocar aquí tu lógica)
-                  },
-                ),
-              ],
-            ),
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // Acción al presionar el botón de búsqueda
+            },
           ),
         ],
       ),
       body: const ChatScreen(),
       drawer: Drawer(
-        width: 230,
-        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.horizontal(right: Radius.circular(20))),
+        width: 250,
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.horizontal(right: Radius.circular(20)),
+        ),
         child: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
@@ -71,70 +66,80 @@ class _SanteState extends State<Sante> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: ListView(
             children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 20, 20, 20),
-                child: Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.arrow_back_ios_new,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const Text(
-                      pagenow,
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    )
-                  ],
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(15, 2, 20, 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: AssetImage('assets/images/IntroImage.jpg'),
-                    ),
-                    SizedBox(width: 20),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(10, 20, 20, 20),
+                    child: Row(
                       children: [
-                        Text(
-                          'Anonimo',
-                          style: TextStyle(
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back_ios_new,
                             color: Colors.white,
-                            fontSize: 16,
                           ),
                         ),
-                        Text(
-                          'Usuario',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
+                        const Text(
+                          pagenow,
+                          style: TextStyle(color: Colors.white, fontSize: 16),
                         ),
                       ],
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(15, 2, 20, 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.black87,
+                          radius: 32,
+                          child: CircleAvatar(
+                            radius: 30,
+                            backgroundImage: AssetImage('assets/images/profileDefault.jpg'),
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Usuario Anónimo',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              'Usuario',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Divider(color: Colors.white, thickness: 3, indent: 15, endIndent: 15),
+                  TileDrawer('Mi perfil', Icons.account_circle),
+                  TileDrawer('Organizaciones', Icons.group),
+                  TileDrawer('Configuración', Icons.settings),
+                  TileDrawer('Sobre Nosotros', Icons.emoji_people),
+                  TileDrawer('Contáctanos', Icons.corporate_fare),
+                  TileDrawer('Soy Psicólogo', Icons.psychology),
+                ],
               ),
-              const Divider(color: Colors.white10, thickness: 3, indent: 3),
-              TileDrawer('Organizaciones', Icons.group),
-              TileDrawer('Configuracion', Icons.settings),
-              TileDrawer('Sobre Nosotros', Icons.emoji_people),
-              TileDrawer('Contactanos', Icons.corporate_fare),
             ],
           ),
         ),
-      )
-
-      ,
+      ),
     );
   }
 }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,26 +3,26 @@ import 'package:sante2/src/widgets/SectionText.dart';
 import 'package:sante2/src/widgets/SectionTitle.dart';
 
 Widget TextSectionV1(String title, String text) => Column(
-    children: [
-      Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey[300]!),
-            borderRadius: BorderRadius.circular(8),
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey[300]!),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              SectionTitle(title),
+              const SizedBox(height: 16),
+              SectionText(text),
+              const SizedBox(height: 16)
+            ]),
           ),
-          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            SectionTitle(title),
-            const SizedBox(height: 16),
-            SectionText(text),
-            const SizedBox(height: 16)
-          ]),
         ),
-      ),
-
-    ],
-  );
+      ],
+    );
 
 Widget TileDrawer(String title, IconData iconData) {
   return ListTile(
@@ -36,7 +36,3 @@ Widget TileDrawer(String title, IconData iconData) {
     ),
   );
 }
-
-
-
-
